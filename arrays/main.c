c1bb9daf3782713f62329e4c3f58f5ef060d7c55
Saim Khan <saimkhan065@gmail.com>
2023-03-12 13:28:44 -0400

    added array struct and display func prototype

--- arrays/main.c
+++ arrays/main.c
@@ -1,6 +1,16 @@
+/*
+ * objective : implement arrays using struct
+ */
 #include <stdio.h>
+struct Array{
+    int *pointer_to_arr;
+    int arr_size;
+    int arr_length;
+
+};
+void displayArray(struct Array arr);
 
 int main() {
-    printf("Hello, World!\n");
+    
     return 0;
 }
