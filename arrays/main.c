ad7e2bbdd90f7a97c4fcb1488f78ea96cd250b10
Saim Khan <saimkhan065@gmail.com>
2023-03-12 13:50:45 -0400

    save before going CC

--- arrays/main.c
+++ arrays/main.c
@@ -1,16 +1,26 @@
 /*
  * objective : implement arrays using struct
  */
+
 #include <stdio.h>
-struct Array{
+#include <stdlib.h>
+struct Array{ // array struct
     int *pointer_to_arr;
     int arr_size;
     int arr_length;
 
 };
-void displayArray(struct Array arr);
+void displayArray(struct Array arr); // display arr contents
 
 int main() {
-    
+    struct Array arr;
+    arr.arr_size = 10; //desired arr size for this example
+    arr.pointer_to_arr = (int *)malloc(arr.arr_size * sizeof (int)); //allocate memory and point to address
+    arr.arr_length = 0; //arr is empty
+
+    for(int i=0; i<10; i++){ //taking 10 elements in arr as example
+        arr.pointer_to_arr[i] = i;
+    }
+
     return 0;
 }
