95513cb04d5d71cc2cb2a0b8ad075edef621f0f9
Saim Khan <saimkhan065@gmail.com>
2023-03-12 17:49:36 -0400

    added display function

--- arrays/main.c
+++ arrays/main.c
@@ -18,9 +18,17 @@ int main() {
     arr.pointer_to_arr = (int *)malloc(arr.arr_size * sizeof (int)); //allocate memory and point to address
     arr.arr_length = 0; //arr is empty
 
-    for(int i=0; i<10; i++){ //taking 10 elements in arr as example
+    for(int i=0; i<arr.arr_size; i++){ //taking 10 elements in arr as example
         arr.pointer_to_arr[i] = i;
+        arr.arr_length++; //increment length as element is added in array
     }
 
+    displayArray(arr);
     return 0;
+}
+
+void displayArray(struct Array arr){
+    for(int i=0; i<arr.arr_length; i++){
+        printf("%d\n", arr.pointer_to_arr[i]);
+    }
 }
