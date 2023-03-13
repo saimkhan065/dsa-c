18eb8200fd9ba0bcfb481920c15b6e02e25a7442
Saim Khan <saimkhan065@gmail.com>
2023-03-13 15:23:16 -0400

    added delete function

--- arrays/main.c
+++ arrays/main.c
@@ -12,22 +12,23 @@ struct Array{ // array struct
 };
 void displayArray(struct Array arr); // display arr contents
 void insert_in_array(struct Array arr, int index, int element); //insert element in array at given index
+void delete_from_array(struct Array arr, int index); //delete element at index
 
 int main() {
     struct Array arr;
-    arr.arr_size = 11; //desired arr size for this example - changed from 10 to 11 for one insert
+    arr.arr_size = 20; //changed to 20 to accommodate more example operations
     arr.pointer_to_arr = (int *)malloc(arr.arr_size * sizeof (int)); //allocate memory and point to address
     arr.arr_length = 0; //arr is empty
 
-    for(int i=0; i<arr.arr_size; i++){ //taking 10 elements in arr as example
+    for(int i=0; i<arr.arr_size-10; i++){ //taking 10 elements in arr as example
         arr.pointer_to_arr[i] = i;
         arr.arr_length++; //increment length as element is added in array
     }
-
+//Print Statements
+    printf("Basic array length: %d\n",arr.arr_length);
     displayArray(arr);
     insert_in_array(arr, 1, 99);
-    printf("\n");
-    displayArray(arr);
+    delete_from_array(arr, 1);
     return 0;
 }
 
@@ -38,10 +39,20 @@ void displayArray(struct Array arr){
 }
 
 void insert_in_array(struct Array arr, int index, int element){
-    arr.arr_size = arr.arr_size++; // increase size - example already has 10 elements populated
-    for(int i=arr.arr_length; i>=index; i--){
-        arr.pointer_to_arr[i] = arr.pointer_to_arr[i-1]; //move element at index by 1
+    for(int i=arr.arr_length; i>index; i--){
+        arr.pointer_to_arr[i] = arr.pointer_to_arr[i-1]; //shift elements in array
     }
     arr.pointer_to_arr[index] = element;
     arr.arr_length++;
+    printf("\nLength after insertion: %d\n", arr.arr_length);
+    displayArray(arr);
+}
+
+void delete_from_array(struct Array arr, int index){
+    for(int i=index; i<arr.arr_length-1; i++){
+        arr.pointer_to_arr[i] = arr.pointer_to_arr[i+1]; //shift elements back starting from index to overwrite
+    }
+    arr.arr_length--;
+    printf("\nLength after deletion: %d\n", arr.arr_length);
+    displayArray(arr);
 }
