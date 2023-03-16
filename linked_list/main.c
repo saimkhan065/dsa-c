58b3d52078b81957d1d14bf1ee82e43e511e35d5
Saim Khan <saimkhan065@gmail.com>
2023-03-16 17:24:49 -0400

    added objective and struct node

--- linked_list/main.c
+++ linked_list/main.c
@@ -1,4 +1,13 @@
+/*
+ * Objective: 1. implement linked list
+ */
 #include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int element;
+    struct node *next_node_pointer;
+}*first_node_pointer;
 
 int main() {
     printf("Hello, World!\n");
