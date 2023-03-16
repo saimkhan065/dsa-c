539bc24bcd67e5827b578bfbe6e4c5d1a0e6ddc1
Saim Khan <saimkhan065@gmail.com>
2023-03-16 17:40:53 -0400

    added display linked list function

--- linked_list/main.c
+++ linked_list/main.c
@@ -10,11 +10,13 @@ struct node {
 }*first_node_pointer=NULL;
 
 void create_node(int a[], int n);
+void display_linked_list(struct node *p);
 
 
 int main() {
     int a[] = {1, 2, 3, 4, 5};
-
+    create_node(a, 5);
+    display_linked_list(first_node_pointer);
     return 0;
 }
 
@@ -31,5 +33,12 @@ void create_node(int a[], int n){
         temporary_node_pointer->next_node_pointer = NULL;
         last_node_pointer->next_node_pointer = temporary_node_pointer;
         last_node_pointer = temporary_node_pointer;
+    }
+}
+
+void display_linked_list(struct node *p){
+    while(p){
+        printf("%d\t", p->element);
+        p=p->next_node_pointer;
     }
 }
