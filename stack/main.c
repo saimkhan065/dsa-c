bd235e0cf382cf669ef5dfe2bff0d48d77c58d9c
Saim Khan <saimkhan065@gmail.com>
2023-03-20 12:17:50 -0400

    added create/display stack functions

--- stack/main.c
+++ stack/main.c
@@ -2,8 +2,32 @@
  * objective 1: implement stack
  */
 #include <stdio.h>
+#include <stdlib.h>
+
+struct stack{
+    int top;
+    int size;
+    int *pointer_to_stack_array;
+};
+void create_stack(struct stack *stack); //initialise a stack
+void display_stack(struct stack stack); //display stack
+void push_stack(struct stack *stack, int element); // push an element in stack
 
 int main() {
     printf("Hello, World!\n");
     return 0;
 }
+
+void create_stack(struct stack *stack){
+    stack->size = 10; //sample size 10
+    stack->top = -1; // stack is empty
+    stack->pointer_to_stack_array = (int *)malloc(stack->size * sizeof(int)); //int pointer allocate memory
+}
+
+void display_stack(struct stack stack){
+    for(int i=0; i>=0; i--){
+        printf("%d\n", stack.pointer_to_stack_array[i]);
+    }
+}
+
+
