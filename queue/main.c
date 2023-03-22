e50a8415371bce350d52c1af114253c5387df472
Saim Khan <saimkhan065@gmail.com>
2023-03-22 11:50:55 -0400

    added create queue function

--- queue/main.c
+++ queue/main.c
@@ -1,9 +1,28 @@
 /*
- * Objective: 1. implement queue 
+ * Objective: 1. implement queue
  */
 #include <stdio.h>
+#include <stdlib.h>
+
+struct queue{
+    int size;
+    int *pointer_to_array;
+    int front;
+    int rear;
+};
+
+void create_queue(struct queue *q, int size);
 
 int main() {
-    printf("Hello, World!\n");
+    struct queue q;
+    create_queue(&q, 10);
     return 0;
+}
+
+void create_queue(struct queue *q, int size){
+    q->size = size;
+    q->front = -1;
+    q->rear = -1;
+    q->pointer_to_array = (int *) malloc(q->size * sizeof(int));
+    printf("function run success!"); // check to see if any error rn
 }
