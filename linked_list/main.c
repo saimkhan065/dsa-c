ea3cdc8ebd65b8cdfec003aeead84c97d6c73940
Saim Khan <saimkhan065@gmail.com>
2023-06-05 16:05:46 -0400

    still did not understand LL. hopefully this time ... :')

--- linked_list/main.c
+++ linked_list/main.c
@@ -4,49 +4,7 @@
 
 
 
-
-typedef struct Node{
-    int data;
-    struct Node *ptrNextNode;
-}Node, *NodePtr;
-
-NodePtr createNode(int element);
-NodePtr addNode(int element);
-void displayNode(NodePtr nodePtr);
-
 int main() {
-    int listSize;
-    NodePtr firstNode, secondNode, thirdNode;
-    firstNode = createNode(10);
-    secondNode = createNode(20);
-    firstNode->ptrNextNode = secondNode;
-    thirdNode = createNode(30);
-    secondNode->ptrNextNode = thirdNode;
 
-    displayNode(firstNode);
-    displayNode(secondNode);
-    displayNode(thirdNode);
     return 0;
-}
-
-NodePtr createNode(int element){
-   NodePtr ptr;
-    ptr = (NodePtr) malloc(sizeof (Node));
-    ptr->data = element;
-    ptr->ptrNextNode = NULL;
-    return ptr;
-}
-
-void displayNode(NodePtr nodePtr){
-    printf("Data at Node: %d\n", nodePtr->data);
-    printf("Current Addr: %p\n", nodePtr);
-    printf("Next Addr: %p\n", nodePtr->ptrNextNode);
-}
-
-NodePtr addNode(int element){
-    NodePtr ptr;
-    ptr = (NodePtr) malloc(sizeof (Node));
-    ptr->data = element;
-    ptr->ptrNextNode = NULL;
-    return ptr;
 }
